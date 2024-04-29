1bec61a1de88db8c500df80a12604a7979c94819
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-29 09:15:46 -0400

    initial commit - installed packages/dependencies

---
+++ src/app/pages/_app.js
@@ -0,0 +1,12 @@
+// app page layout
+import React from 'react'
+import '@radix-ui/themes'
+import '../globals.css'
+
+const _app = () => {
+  return (
+    <div>_app</div>
+  )
+}
+
+export default _app
---
+++ src/app/pages/index.js
@@ -0,0 +1,10 @@
+// home page
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
