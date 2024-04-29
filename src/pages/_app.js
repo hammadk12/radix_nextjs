bf970a42d6fa5ac923693f01335cbda8282c36c0
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-29 11:48:32 -0400

    STYLED PAGE - added background image - designed figma UI - installed react-icons from radix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,17 +4,14 @@ import Layout from '../layout'
 import Navbar from '@/components/Navbar'
 import '@radix-ui/themes'
 import '../globals.css'
-import { Theme } from '@radix-ui/themes'
 
 const _app = ({ Component, pageProps }) => {
   return (
-    <Theme accentColor='crimson' grayColor='sand' radius='large' scaling='95%'>
-      <div>
+      <div className='text-white'>
         <Navbar />
         <Component {...pageProps} />
         <Layout />
       </div>
-    </Theme>
   )
 }
 
