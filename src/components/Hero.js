fd35d5e3a24a21104dfe20365723a7e0e077f121
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-05-01 08:31:14 -0400

    INFO SECTION DEVELOPMENT - in InfoSection.js: - implemented grid layout - imported Grid component from Radix - utilized background sizing to ensure full screen responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='p-10 mb-[300px] md:mb-[340px] lg:mb-[280px] card-bg m-10 md:m-20 lg:mx-[400px] rounded-3xl'>
+    <div className='p-10 mb-[280px] mt-[120px] md:mb-[260px] card-bg m-10 md:mx-20 lg:mb-[240px] lg:mx-[400px] lg:mt-[80px] rounded-3xl'>
         <div className='text-[#FAF0E6] text-center m-auto'>
             <p className='text-4xl md:text-6xl lg:text-7xl lg:leading-[80px] font-semibold'>Become the <span className='font-extrabold'>best</span> version of <br></br>yourself with HTrain.</p>
             <p className='text-xl md:text-2xl lg:text-3xl font-semibold'>Stop guessing in the gym. Find what <br></br> works for you here. <span className='font-extrabold'>No BS.</span></p>
--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { Flex, Box } from '@radix-ui/themes'
+import { Grid } from '@radix-ui/themes'
 
 const InfoSection = () => {
   return (
-    <section className='bg-white flex flex-col h-fit'>
-        <p>hi</p>
-      
-    </section>
+    <div className='mx-10 md:mx-20 lg:mx-[200px] bg-white rounded-3xl'>
+      <Grid
+        className='grid md:grid-flow-row w-full h-[100vh] text-center md:gap-x-40 p-12'
+        columns={{ sm: '1fr', md: '1fr 1fr' }}
+        rows="1fr 1fr 1fr"
+        // style={{ gridTemplateColumns: '1fr' }}
+      >
+        <div className=''>
+          <h2 className='text-3xl lg:text-5xl'>Everything in one place.</h2>
+        </div>
+        <div className='md:flex justify-center items-center '>
+          <h4>Choose your experience level, your goal, and tell us what you like. It’s as simple as that.</h4>
+        </div>
+        <div className=''>
+          <h2 className='text-3xl lg:text-5xl'>Get started in minutes.</h2>
+        </div>
+        <div className='md:flex justify-center items-center '>
+          <h4>After HTrain processes your goals, you’ll receive a custom plan just for you.</h4>
+        </div>
+        <div className=''>
+          <h2 className='text-3xl lg:text-5xl'>Reach your goals.</h2>
+        </div>
+        <div className='md:flex justify-center items-center '>
+          <h4>Track your progress, reach your goals, and achieve what you always wanted.</h4>
+        </div>
+      </Grid>
+      </div>
   )
 }
 
 export default InfoSection
-// mt-[300px] md:mt-[340px] lg:mt-[280px]
