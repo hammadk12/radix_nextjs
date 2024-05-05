dd80a263b2dcae930b524a1ec21f3ed720af5d74
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-05-05 11:26:38 -0400

    Rough Draft Footer Complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    const current_year = new Date().getFullYear();
+  
+    return (
+      <div>
+      <div className='w-full'>
+        <div className='text-center py-10 font-bold '>
+          <Link href="#"  className='px-1 no-underline'>
+            Privacy
+          </Link>
+          <span>|</span>
+          <Link href="#"  className='px-1 no-underline'>
+            Terms
+          </Link>
+          <span>|</span>
+          <Link href='#' className='px-1 no-underline'>
+            Contact
+          </Link>
+          <p>Copyright © {current_year}, HTrain. All Rights Reserved.</p>
+        </div>
+      </div>
+      </div>
+    )
 }
 
 export default Footer
