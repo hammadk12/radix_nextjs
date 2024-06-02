12b63b76d9f41c63eff7318c5068199c4995e13c
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-06-02 13:53:36 -0400

    Configured postcss.config & tailwind.config with purgeCSS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,18 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
+    ...AbortController(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: [
+              "./src/pages/**/*.{js,jsx,ts,tsx}",
+              "./src/components/**/*.{js,jsx,ts,tsx}",
+            ],
+            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+      }
+      : {}),
   },
 };
 
