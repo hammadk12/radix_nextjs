c7a40d122901bc61735c662622079dc00ac5a466
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-06-02 19:28:42 -0400

    Reverted back to commit for debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,53 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    webpack: true,
-
-    // Using the postcss-loader
-    webpack(config, { dev, isServer }) {
-    if (!dev && !isServer) {
-      // Modify the PostCSS configuration to include Tailwind CSS and PurgeCSS
-      const postcssLoader = config.module.rules.find(
-        (rule) => rule.oneOf && rule.oneOf.find((innerRule) => innerRule.sideEffects === false)
-      );
-
-      if (postcssLoader) {
-        const originalPostcssLoaderUse = postcssLoader.use;
-        postcssLoader.use = [
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                ident: 'postcss',
-                plugins: [
-                  'tailwindcss',
-                  'autoprefixer',
-                  ...(process.env.NODE_ENV === 'production'
-                    ? [
-                        [
-                          '@fullhuman/postcss-purgecss',
-                          {
-                            content: [
-                              './src/pages/**/*.{js,jsx,ts,tsx}',
-                              './src/components/**/*.{js,jsx,ts,tsx}',
-                              // Add more paths as necessary
-                            ],
-                            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-                          },
-                        ],
-                      ]
-                    : []),
-                ],
-              },
-            },
-          },
-          ...originalPostcssLoaderUse,
-        ];
-      }
-    }
-
-    return config;
-  },
+    
 };
 
 module.exports = nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,18 +2,7 @@
 const config = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    ...AbortController(process.env.NODE_ENV === "production"
-      ? {
-          "@fullhuman/postcss-purgecss": {
-            content: [
-              "./src/pages/**/*.{js,jsx,ts,tsx}",
-              "./src/components/**/*.{js,jsx,ts,tsx}",
-            ],
-            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-          }
-      }
-      : {}),
+    autoprefixer: {}
   },
 };
 
