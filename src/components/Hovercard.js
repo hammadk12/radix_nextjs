a7ff5cfee1dabbb67570618f62a8ab59121edc71
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-06-06 09:58:43 -0400

    Modified hovercard component for mobile friendly approach

--- src/components/Hovercard.js
+++ src/components/Hovercard.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, Flex, Link, Box, HoverCard, Strong } from '@radix-ui/themes'
 
 const Hovercard = ({ exercise }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 768px)').matches)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 768px)')
+    const handleResize = () => setIsMobile(mediaQuery.matches)
+
+    mediaQuery.addEventListener('change', handleResize)
+    return () => mediaQuery.removeEventListener(handleResize)
+  }, [])
+
+  const handleCardClick = () => {
+    if (isMobile) setIsVisible(!isVisible)
+  }
+
   return (
-    <Text>
+    <Text onClick={handleCardClick}>
       {' '}
-        <HoverCard.Root>
+        <HoverCard.Root open={isMobile ? isVisible : undefined}>
             <HoverCard.Trigger>
-             <Link size='5' href={exercise.Video} target='_blank'>{exercise.WorkOut}</Link>
+              <Text className='text-[#baa7ff] cursor-pointer'>{exercise.WorkOut}</Text>
             </HoverCard.Trigger>
             <HoverCard.Content className='w-[350px] p-6 mr-14'>
             <Flex gap='4'>
               <Box className='text-black'>
                 <ul>
-                <li><Text><Strong>Click on {exercise.WorkOut} for a video tutorial!</Strong></Text></li>
                 <li><Text><Strong>Equipment:</Strong> {exercise.Equipment}</Text></li>
                 <li><Text><Strong>Intensity Level:</Strong> {exercise.Intensity_Level}</Text></li>
                 <li><Text><Strong>Explanation:</Strong> {exercise.Explaination}</Text></li>
@@ -21,6 +35,7 @@ const Hovercard = ({ exercise }) => {
                 <li><Text><Strong>Intermediate Sets:</Strong> {exercise["Intermediate Sets"]}</Text></li>
                 <li><Text><Strong>Expert Sets:</Strong> {exercise["Expert Sets"]}</Text></li>
                 <li><Text><Strong>Long Explanation:</Strong> {exercise["Long Explanation"]}</Text></li>
+                <li><Text><Strong>Video: </Strong><Link size='4' className='underline text-[#8668ffcc]' href={exercise.Video} target='_blank'>{exercise.WorkOut}</Link></Text></li>
                 </ul>
                 </Box>
             </Flex>
